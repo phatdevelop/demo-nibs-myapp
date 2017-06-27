4ca9ff6b5b8b7d1d75385e2fb2d564112b8ed4b0
ytakemoto <ytakemoto@keizu.co.jp>
2017-06-27 22:10:01 +0900

    init.sql add Faq__kav

--- init.sql
+++ init.sql
@@ -22,5 +22,42 @@ CREATE TABLE IF NOT EXISTS picture (
     publishDate  timestamp default current_timestamp
   );
 
+CREATE TABLE IF NOT EXISTS Faq__kav (
+    id                      BIGSERIAL PRIMARY KEY,
+    KnowledgeArticleId      TEXT,
+    OwnerId                 TEXT,
+    IsDeleted               TEXT,
+    PublishStatus           TEXT,
+    VersionNumber           double precision,
+    IsLatestVersion         TEXT,
+    IsVisibleInPkb          TEXT,
+    IsVisibleInCsp          TEXT,
+    IsVisibleInPrm          TEXT,
+    CreatedDate             DATE,
+    CreatedById             TEXT,
+    LastModifiedDate        DATE,
+    LastModifiedById        TEXT,
+    SystemModstamp          DATE,
+    Language                TEXT,
+    Title                   TEXT,
+    UrlName                 TEXT,
+    ArchivedDate            DATE,
+    ArchivedById            TEXT,
+    Summary                 TEXT,
+    ArticleNumber           TEXT,
+    FirstPublishedDate      DATE,
+    LastPublishedDate       DATE,
+    ArticleArchivedById     TEXT,
+    ArticleArchivedDate     DATE,
+    ArticleCaseAttachCount  double precision,
+    ArticleCreatedById      TEXT,
+    ArticleCreatedDate      DATE,
+    ArticleMasterLanguage   TEXT,
+    ArticleTotalViewCount   double precision,
+    SourceId                TEXT,
+    ArticleType             TEXT,
+    ArticleBody__c          TEXT
+  );
+
 
 
