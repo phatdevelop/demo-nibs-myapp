bac566881586115b0c98ae15872e4978a01fbe67
ttkhai <ttkhai@keizu.co.jp>
2017-07-14 15:25:35 +0700

    test

--- client/js/product.js
+++ client/js/product.js
@@ -1,4 +1,4 @@
-angular.module('nibs.product', ['openfb', 'openline', 'nibs.status', 'nibs.activity', 'nibs.wishlist'])
+angular.module('nibs.product', ['openfb', 'nibs.status', 'nibs.activity', 'nibs.wishlist'])
 
     .config(function ($stateProvider) {
 
