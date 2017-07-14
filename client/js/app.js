f12bf5036d8a0726581a9a7b68ac70a7f075d390
ttkhai <ttkhai@keizu.co.jp>
2017-07-14 17:05:55 +0700

    test

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('nibs', ['ionic', 'openfb', 'nibs.config', 'nibs.profile', 'nibs.auth', 'nibs.product', 'nibs.offer', 'nibs.store-locator', 'nibs.gallery', 'nibs.settings', 'nibs.case'])
 
-    .run(function ($window, $location, $rootScope, $state, $ionicPlatform, $http, OpenFB, FB_APP_ID, LINE_CHANNEL_ID, SERVER_URL) {
+    .run(function ($window, $location, $rootScope, $state, $ionicPlatform, $http, OpenFB, OpenLINE, FB_APP_ID, LINE_CHANNEL_ID, SERVER_URL) {
 
         var user = JSON.parse($window.localStorage.getItem('user'));
 
