5edd50b622e5bec94ba2783261c2942908873778
TranThanhKhai <ttkhai@keizu.co.jp>
2017-07-18 21:51:54 +0700

    t

--- server/line.js
+++ server/line.js
@@ -12,22 +12,51 @@ function login(req, res, next) {
 	var userId = user.userId;
     var token = req.body.token;
 
-    function createAndSendToken(user) {
-        console.log('send token for user:' + JSON.stringify(user));
-        auth.createAccessToken(user)
-            .then(function(token) {
-                var response = {'user':{'email': user.email, 'firstName': user.firstname, 'lastName': user.lastname}, 'token': token};
-                winston.info(JSON.stringify(response));
-                return res.send(response);
-            })
-            .catch(next);
-    }
+    // function createAndSendToken(user) {
+    //     console.log('send token for user:' + JSON.stringify(user));
+    //     auth.createAccessToken(user)
+    //         .then(function(token) {
+    //             var response = {'user':{'email': user.email, 'firstName': user.firstname, 'lastName': user.lastname}, 'token': token};
+    //             winston.info(JSON.stringify(response));
+    //             return res.send(response);
+    //         })
+    //         .catch(next);
+    // }
 
-    // Check if Line token is valid and matches the Line User id provided.
-    validateLINEToken(token, userId)
-        .then(function () {
-            // The Line token is valid
-            db.query('SELECT id, firstName, lastName, email, loyaltyid__c as externalUserId FROM salesforce.contact WHERE lineUserId__c=$1', [userId], true)
+    // // Check if Line token is valid and matches the Line User id provided.
+    // validateLINEToken(token, userId)
+    //     .then(function () {
+    //         // The Line token is valid
+    //         db.query('SELECT id, firstName, lastName, email, loyaltyid__c as externalUserId FROM salesforce.contact WHERE lineUserId__c=$1', [userId], true)
+    //             .then(function (user) {
+    //                 if (user) {
+    //                     // The Line user is known
+    //                     // Create a token and send it to the client.
+    //                     winston.info('Known Line user');
+    //                     return createAndSendToken(user);
+    //                 } else {
+    //                     db.query('SELECT id, firstName, lastName, email FROM salesforce.contact WHERE email=$1', [userId], true)
+    //                         .then(function (user) {
+    //                             if (user) {
+    //                                 // We already have a user with that email address
+    //                                 // Add Line id to user record
+    //                                 winston.info('We already have a user with that email address.');
+    //                                 //updateUser(user, user.id).then(createAndSendToken).catch(next);
+    //                             } else {
+    //                                 // First time this Line user logs in (and we don't have a user with that email address)
+    //                                 // Create a user
+    //                                 winston.info('First time this Line user logs in');
+    //                                 //createUser(user).then(createAndSendToken).catch(next);
+    //                             }
+    //                         })
+    //                         .catch(next);
+    //                 }
+    //             })
+    //             .catch(next);
+    //     })
+    //     .catch(next);
+
+    db.query('SELECT id, firstName, lastName, email, loyaltyid__c as externalUserId FROM salesforce.contact WHERE lineUserId__c=$1', [userId], true)
                 .then(function (user) {
                     if (user) {
                         // The Line user is known
@@ -53,37 +82,35 @@ function login(req, res, next) {
                     }
                 })
                 .catch(next);
-        })
-        .catch(next);
 }
 
-function validateLINEToken(token, userId) {
-    winston.info("Validating Line token: " + token + " userId: " + userId);
-    var deferred = Q.defer();
+// function validateLINEToken(token, userId) {
+//     winston.info("Validating Line token: " + token + " userId: " + userId);
+//     var deferred = Q.defer();
 
-    var url = 'https://api.line.me/v2/oauth/verify?access_token=' + token;
+//     var url = 'https://api.line.me/v2/oauth/verify?access_token=' + token;
 
-    https.post(url ,function(res) {
-    	var body = '';
+//     https.post(url ,function(res) {
+//     	var body = '';
 
-    	res.on('end', function() {
-    		var data = JSON.parse(body);
-            winston.info("Line response: " + body);
-            if (data && data.id && data.id === userId) {
-                winston.info("Line token validated");
-                deferred.resolve();
-            } else {
-                winston.error("Error validating Line Token: " + body);
-                deferred.reject();
-            }
-    	});
-    })
-    .on('error', function (e) {
-            winston.error("System error validating Line Token: " + e);
-            deferred.reject(e);
-        });
+//     	res.on('end', function() {
+//     		var data = JSON.parse(body);
+//             winston.info("Line response: " + body);
+//             if (data && data.id && data.id === userId) {
+//                 winston.info("Line token validated");
+//                 deferred.resolve();
+//             } else {
+//                 winston.error("Error validating Line Token: " + body);
+//                 deferred.reject();
+//             }
+//     	});
+//     })
+//     .on('error', function (e) {
+//             winston.error("System error validating Line Token: " + e);
+//             deferred.reject(e);
+//         });
 
-    return deferred.promise;
-}
+//     return deferred.promise;
+// }
 
 exports.login = login;
