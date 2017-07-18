ba3696181abe3864d75cf1b1959f229262b2dace
TranThanhKhai <ttkhai@keizu.co.jp>
2017-07-18 23:02:21 +0700

    t

--- client/js/openline-angular.js
+++ client/js/openline-angular.js
@@ -174,6 +174,7 @@ angular.module('openline', [])
               };
               xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
               xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://demo-nibs-myapp-k.herokuapp.com');
+              xhr.setRequestHeader('Access-Control-Allow-Credentials', 'true');
               xhr.withCredentials = true;
               var params = 'grant_type=authorization_code&client_id=' + channelId + '&client_secret=' + channelSecret + '&code=' + authorizationCode + '&redirect_uri=' + callbackURL;
               xhr.send(params);
